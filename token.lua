7721be4100c416c2762c8c0f405170f58c173daf
Tanner Grehawick <tgrehawi@gmail.com>
2016-09-26 00:54:25 -0400

    lots of tokenizer work
    * token type definitions are a lot simpler now
    * tokenizer is greedy (it tokenizes the last type that matches successfully instead of the first)
    + identifier and integer literal token types
    + more keywords and operators
    * can now parse multicharacter operators
    * tokenizer is more effecient (it removes token types from pool after they have failed or have already matched something)
    
    todo: comment and clean this shit

--- token.lua
+++ token.lua
@@ -4,18 +4,16 @@ token = {}
 token.patterns = {}
 
 token.type = setmetatable({}, {
-	__call = function(self, name, text, finalpattern)
+	__call = function(self, name, value, finalpattern)
 		local t = {}
 		t.__index = t
 		function t:__tostring()
-			return string.format("token[%s (%s)]", self.text, self.type.name)
+			return string.format("token[%s (%s)]", self.value, self.type.name)
 		end
 		t.type = t
 		t.name = name
 		t.finalpattern = finalpattern
-		t.text = text
-		token.types[name] = t
-		table.insert(token.types, t)
+		t.value = value
 		return setmetatable(t, self)
 	end
 }) do
@@ -24,25 +22,29 @@ token.type = setmetatable({}, {
 
 	tt.__index = tt
 
-	tt.finalpattern = "[^%w]"
+	tt.finalpattern = "[^%w_]"
 
 	--prepare to start matching this token type
 	function tt:startmatching()
-		self.i = 0
+		if self.iscustom then
+			self.value = nil
+		else
+			self.i = 0
+		end
 	end
 
 	--process next character. return nothing if the character is valid for this token type,
 	-- true if it is invalid and has therefore disqualified this token type,
 	-- or a table representing the fully parsed token if the next character finalizes it
 	function tt:nextchar(char)
-		if self.i == #self.text then
+		if self.i == #self.value then
 			if char:match(self.finalpattern) then
 				return self()
 			else
 				return true
 			end
 		else
-			if char == self.text:sub(self.i + 1, self.i + 1) then
+			if char == self.value:sub(self.i + 1, self.i + 1) then
 				self.i = self.i + 1
 			else
 				return true
@@ -51,20 +53,42 @@ token.type = setmetatable({}, {
 	end
 
 	function tt:__tostring()
-		return string.format("tokentype[%s (%s)]", self.name, self.text)
+		return string.format("tokentype[%s (%s)]", self.name, self.value)
 	end
 
-	--initialize a new token instance of this token type, with the passed text as that token's text.
-	function tt:__call(text)
+	--initialize a new token instance of this token type, with the passed value as that token's value.
+	function tt:__call(value)
 		local token = setmetatable({
-			text = text
+			value = value
 		}, self)
 		return token
 	end
 
 end
 
-token.types = {}
+token.types = setmetatable({}, {
+	__newindex = function(self, k, v)
+		if type(k) == "string" then
+			local tokentype
+			if type(v) == "string" then
+				if v == "keyword" then
+					tokentype = token.type(v, k)
+					rawset(self, k, tokentype)
+				elseif v:match("operator") then
+					tokentype = token.type(v, k, ".")
+					rawset(self, v, tokentype)
+				end
+			elseif type(v) == "function" then
+				tokentype = token.type(k, "")
+				tokentype.nextchar = v
+				tokentype.iscustom = true
+				rawset(self, k, tokentype)
+			end
+		else
+			rawset(self, k, v)
+		end
+	end
+})
 
 --returns an iterator that scans through a file and returns individual token objects
 -- iterator also returns line, column count after each successful token
@@ -91,29 +115,41 @@ function token.tokenize(file)
 		if not next then return end
 		local sl, sc = l, c	--line and column start counts for this token
 		local fragment = ""
-		local candididates = #token.types
-		for _, type in ipairs(token.types) do
+		local candidates = {}
+		local remaining = 0
+		for _, type in pairs(token.types) do
 			type:startmatching()
+			candidates[type] = true
+			remaining = remaining + 1
 		end
-		local failures = {}
+		local lastmatch, lastsl, lastsc, lastl, lastc
 		while next do
 			fragment = fragment..next
-			for _, type in ipairs(token.types) do
-				if not failures[type] then
-					local result = type:nextchar(next)
-					if result == true then
-						failures[type] = true
-						candididates = candididates - 1
-					else
-						if result then
+			for type in pairs(candidates) do
+				local result = type:nextchar(next)
+				if result == true then
+					candidates[type] = nil
+					remaining = remaining - 1
+				else
+					if result then
+						if remaining == 1 then
 							return result, sl, sc, l, c
+						else
+							candidates[type] = nil
+							remaining = remaining - 1
+							lastmatch, lastsl, lastsc, lastl, lastc
+								= result, sl, sc, l, c
 						end
 					end
 				end
 			end
 			getnext()
-			if candididates == 0 then
-				return string.format("could not parse token \'%s\'", fragment), sl, sc, l, c
+			if remaining == 0 then
+				if lastmatch then
+					return lastmatch, lastsl, lastsc, lastl, lastc
+				else
+					return string.format("could not parse token \'%s\'", fragment), sl, sc, l, c
+				end
 			end
 		end
 	end
